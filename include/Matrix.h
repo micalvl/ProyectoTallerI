3e9b99d79f5d1866e635270b972b2a3cc2dcc9a3
micalvl <miguelcalvoleon04@gmail.com>
2025-03-20 15:38:23 +0100

    commit

---
+++ include/Matrix.h
@@ -0,0 +1,33 @@
+//
+// Created by micalvl on 20/03/2025.
+//
+
+#ifndef PROYECTOTALLERI_MATRIX_H
+#define PROYECTOTALLERI_MATRIX_H
+
+
+class Matrix {
+public:
+    Matrix(int fil, int col);
+    Matrix(int fil, int col, double v[], int n);
+    Matrix(const Matrix& m);
+    ~Matrix();
+
+    Matrix& operator=(const Matrix& matrix2);
+    Matrix operator+(const Matrix& matrix2);
+    Matrix operator-(const Matrix& matrix2);
+    Matrix operator*(const Matrix& matrix2);
+    double& operator()(const int i, const int j) const;
+
+    void print();
+
+
+private:
+    void initMatrix();
+
+private:
+
+};
+
+
+#endif //PROYECTOTALLERI_MATRIX_H
