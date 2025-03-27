a6f29bf1824367000677a0ebcb28c2671369cf98
micalvl <miguelcalvoleon04@gmail.com>
2025-03-27 15:56:35 +0100

    Commit matrix documentado

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,6 +1,11 @@
-//
-// Created by micalvl on 20/03/2025.
-//
+/**
+ *  @file   Matrix.h
+ *  @brief  Matrix's methods
+ *  @author [Original Author]
+ *  @transcribed by Miguel Calvo León
+ *  @date   2025-03-27
+ ***********************************************/
+
 
 #ifndef PROYECTOTALLERI_MATRIX_H
 #define PROYECTOTALLERI_MATRIX_H
@@ -8,26 +13,92 @@
 
 class Matrix {
 public:
+    /**
+     * @brief Constructs a matrix with the given dimensions.
+     *
+     * @param fil Number of rows.
+     * @param col Number of columns.
+     */
     Matrix(int fil, int col);
+
+    /**
+     * @brief Constructs a matrix with the given dimensions and initializes it with values from an array.
+     *
+     * @param fil Number of rows.
+     * @param col Number of columns.
+     * @param v Array containing the values to initialize the matrix.
+     * @param n Number of elements in the array.
+     */
     Matrix(int fil, int col, double v[], int n);
+
+    /**
+     * @brief Copy constructor. Creates a new matrix as a copy of an existing one.
+     *
+     * @param m The matrix to copy.
+     */
     Matrix(const Matrix& m);
+
+    /**
+     * @brief Destructor.
+     */
     ~Matrix();
 
+    /**
+     * @brief Assigns the contents of another matrix.
+     *
+     * @param matrix2 The matrix to assign.
+     * @return Reference to the modified matrix.
+     */
     Matrix& operator=(const Matrix& matrix2);
+
+    /**
+     * @brief Sums two matrices.
+     *
+     * @param matrix2 The matrix to add.
+     * @return The resulting matrix.
+     */
     Matrix operator+(const Matrix& matrix2);
+
+    /**
+     * @brief Subtracts another matrix from this matrix.
+     *
+     * @param matrix2 The matrix to subtract.
+     * @return The resulting matrix.
+     */
     Matrix operator-(const Matrix& matrix2);
+
+    /**
+     * @brief Multiplies two matrices.
+     *
+     * @param matrix2 The matrix to multiply.
+     * @return The resulting matrix.
+     */
     Matrix operator*(const Matrix& matrix2);
+
+    /**
+     * @brief Accesses an element of the matrix.
+     *
+     * @param i Row index.
+     * @param j Column index.
+     * @return Reference to the matrix element at (i, j).
+     */
     double& operator()(const int i, const int j) const;
 
+    /**
+     * @brief Prints the matrix to the console.
+     */
     void print();
 
 
 private:
+    /**
+     * @brief Initializes the matrix with default values.
+     */
     void initMatrix();
 
 private:
-    int fil, col, n;
-    double** matrix;
+    int fil, col; // Number of rows and columns.
+    double** matrix; // Pointer to the matrix
 };
 
 
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,40 +1,123 @@
-//
-// Created by micalvl on 20/03/2025.
-//
+/**
+ *  @file   Matrix.cpp
+ *  @brief  Matrix's methods
+ *  @author [Original Author]
+ *  @transcribed by Miguel Calvo León
+ *  @date   2025-03-27
+ ***********************************************/
 
 #include "../include/Matrix.h"
 #include <iostream>
 #include <iomanip>
 
-Matrix::Matrix(int fil, int col) : fil(fil), col(col){
+
+Matrix::Matrix(int fil, int col) : fil(fil), col(col)
+{
     initMatrix();
 }
 
-void Matrix::initMatrix(){
-    matrix = new double*[fil];
-    for (int i = 0; i< fil; i++){
-        matrix[i] = new double[col];
-    }
-
-    for(int i = 0; i < fil; i++){
-        for(int j = 0; j < col; j++){
-            matrix[i][j] = 0.0;
-        }
-    }
-}
-
-Matrix::Matrix(int fil, int col, double v[], int n) : fil(fil), col(col){
+Matrix::Matrix(int fil, int col, double v[], int n): fil(fil), col(col)
+{
     initMatrix();
 
     int k = 0;
 
-    for(int i = 0; i < fil; i++)
-        for (int j = 0; j < col; j++) {
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++){
             if (k < n)
                 matrix[i][j] = v[k++];
             else
                 matrix[i][j] = 0;
         }
-
 }
+
+Matrix::Matrix(const Matrix& m)
+{
+    *this = m;
+}
+
+
+void Matrix::initMatrix()
+{
+    matrix = new double*[fil];
+    for (int i = 0; i < fil; i++)
+        matrix[i] = new double[col];
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            matrix[i][j] = 0.0;
+}
+
+Matrix::~Matrix()
+{
+    for (int i = 0; i < fil; i++)
+        delete[] matrix[i];
+
+    delete[] matrix;
+}
+
+Matrix& Matrix::operator=(const Matrix& matrix2)
+{
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            this->matrix[i][j] = matrix2.matrix[i][j];
+
+    return *this;
+}
+
+Matrix Matrix::operator+(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            result.matrix[i][j] = matrix[i][j] + matrix2.matrix[i][j];
+
+    return result;
+}
+
+Matrix Matrix::operator-(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            result.matrix[i][j] = matrix[i][j] - matrix2.matrix[i][j];
+
+    return result;
+}
+
+Matrix Matrix::operator*(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < this->fil ; i++){
+        for (int j = 0; j < matrix2.col; j++){
+            result.matrix[i][j] = 0;
+            for (int k = 0; k < this->col; k++){
+                result.matrix[i][j] = result.matrix[i][j] + this->matrix[i][k] * matrix2.matrix[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+
+double& Matrix::operator()(const int i, const int j) const
+{
+    return matrix[i-1][j-1];
+}
+
+void Matrix::print()
+{
+    for (int i = 0; i < fil; i++){
+        for (int j = 0; j < col; j++){
+            std::cout << std::fixed << std::setprecision(14) << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
 
